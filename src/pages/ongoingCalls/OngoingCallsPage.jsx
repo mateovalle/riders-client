d1fd0e694b4604f8e7769cd29d78cc02f368ee27
mateovalle <mateo.valle@ing.austral.edu.ar>
2022-06-15 09:49:13 -0300

    bug fixes

--- src/pages/ongoingCalls/OngoingCallsPage.jsx
+++ src/pages/ongoingCalls/OngoingCallsPage.jsx
@@ -2,8 +2,11 @@ import {useQuery} from "@apollo/client";
 import {GET_ONGOING_CALLS} from "../../util/queries/callQueries";
 import OngoingCallCard from "../../components/ongoingCallCard/OngoingCallCard";
 import {GET_CALLER_ACTIVE_RIDES} from "../../util/queries/rideQueries";
+import {CircularProgress} from "@mui/material";
+import {useNavigate} from "react-router-dom";
 
 const OngoingCallsPage = () => {
+    const navigate = useNavigate()
 
     const {loading, data, refetch} = useQuery(GET_ONGOING_CALLS, {
         fetchPolicy: "cache-and-network",
@@ -15,18 +18,17 @@ const OngoingCallsPage = () => {
         pollInterval: 1000,
     });
 
-    if (loading || loading2) return <span>loading ...</span>
+    if (loading || loading2) return <CircularProgress color='primary' />
+    if (data.getActiveCalls.length + data2.getCallerActiveRides.length === 0) return <h3>You dont have any ongoing calls. <span style={{color: '#ee2738'}} onClick={() => navigate('/call')}>Create a new call</span></h3>
     return(
         <div>
-            {data.getActiveCalls.map((data) => {
-                return <OngoingCallCard callData={data} deleteCall={() => refetch()} key={data.id} accepted={false}/>
-            })}
             {data2.getCallerActiveRides.map((data) => {
                 console.log(data.vehicleUsed)
                 return <OngoingCallCard callData={data.call} key={data.id} accepted={true} vehicleUsed={data.vehicleUsed} riderArrivedStartLocation={data.riderArrivedStartLocation}/>
             })}
-
-
+            {data.getActiveCalls.map((data) => {
+                return <OngoingCallCard callData={data} deleteCall={() => refetch()} key={data.id} accepted={false}/>
+            })}
         </div>
     )
 }
